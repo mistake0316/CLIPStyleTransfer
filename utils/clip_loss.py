2c378691a8d84a78a16b1876b9bae0e7bfb0a403
Yi-Dar, Tang <changethewhat@gmail.com>
2021-11-23 13:47:38 +0800

    first commit

---
+++ utils/clip_loss.py
@@ -0,0 +1,17 @@
+import torch
+import clip
+
+class CLIPLoss(torch.nn.Module):
+
+    def __init__(self, img_size):
+        super(CLIPLoss, self).__init__()
+        self.model, self.preprocess = clip.load("ViT-B/32", device="cuda")
+        self.upsample = torch.nn.Upsample(scale_factor=7)
+        self.avg_pool = torch.nn.AvgPool2d(kernel_size=img_size//32)
+        self.norm = self.preprocess.transforms[-1]
+
+    def forward(self, image, text):
+        image = self.avg_pool(self.upsample(image))
+        self.norm(image)
+        similarity = 1 - self.model(image, text)[0] / 100
+        return similarity
